38b58919df970abd3549eaae1e6c1e7002211803
Jayalakshmi Dasari <dasari.jayalakshmi17@gmail.com>
2023-02-18 20:45:28 +1100

    added Steps

---
+++ src/test/java/stepDefinitions/Steps.java
@@ -0,0 +1,67 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Steps {
+	@Given("User Launch Chrome browser")
+	public void user_launch_chrome_browser() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User opens URL {string}")
+	public void user_opens_url(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User enters Email as {string} and Password as {string}")
+	public void user_enters_email_as_and_password_as(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Click on Login")
+	public void click_on_login() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Page Title should be {string}")
+	public void page_title_should_be(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I want to write a step with name1")
+	public void i_want_to_write_a_step_with_name1() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I check for the {int} in step")
+	public void i_check_for_the_in_step(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I verify the success in step")
+	public void i_verify_the_success_in_step() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I want to write a step with name2")
+	public void i_want_to_write_a_step_with_name2() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I verify the Fail in step")
+	public void i_verify_the_fail_in_step() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
